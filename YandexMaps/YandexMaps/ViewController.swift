909a75fd0e42ff1bfa45ef07669fb57203f6bff2
levonyan.t <levonyan.t@bk.ru>
2022-12-08 19:30:45 +0300

    initial commit

---
+++ YandexMaps/YandexMaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YandexMaps
+//
+//  Created by Тигран Левонян on 08.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
